b732ab2e606d483f1384b3448546f942ce3b792d
rony quail <ronymarinequail@hotmail.com>
2018-08-11 15:37:25 +0300

    Initial Commit

---
+++ Hello Word/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello Word
+//
+//  Created by RastaOnAMission on 11/08/2018.
+//  Copyright © 2018 rony quail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
